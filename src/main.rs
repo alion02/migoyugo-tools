da00e7a9eab4a5b56234a87e107432eb17919b94
alion02 <alojczek@gmail.com>
2026-01-12 20:33:14 +0100

    Implement make

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![feature(portable_simd)]
+#![allow(
+    clippy::missing_transmute_annotations, // don't care
+)]
 
 pub mod state;
 
--- src/state.rs
+++ src/state.rs
@@ -10,50 +10,41 @@ pub struct Frame {
 
 pub fn make(stack: MultiMut<Frame>, mv: u8) -> MakeResult {
     const DIRECTIONS: u64x4 = Simd::from_array([1, 9, 7, 8]);
-    let [ref p, ref c, ref mut n] = *stack.as_array(-1);
+    let [ref p, ref c] = *stack.as_array(-1);
     let bit = 1 << mv;
-    let mut new_migo = p.opp_migo | bit;
-    let mut new_yugo = p.opp_yugo;
-    let mut new_score = c.score;
-    let mut masks = Simd::splat(new_migo | new_yugo);
+    let mut migo = p.opp_migo | bit;
+    let mut yugo = p.opp_yugo;
+    let mut score = c.score;
+    let mut masks = Simd::splat(migo | yugo);
     masks &= masks >> DIRECTIONS;
     masks &= masks >> DIRECTIONS >> DIRECTIONS;
     masks &= Simd::from_array([0x1F1F1F1F1F1F1F1F, 0x0000001F1F1F1F1F, 0x000000FFFFFFFFFF, 0x000000F8F8F8F8F8]);
-    let line_4 = masks;
-    if line_4.reduce_or() == 0 {
-        n.opp_migo = new_migo;
-        n.opp_yugo = new_yugo;
-        n.score = c.score;
-        // todo check board fill? maybe only in search
-        return MakeResult::Ok;
+    'b: {
+        if masks.reduce_or() == 0 {
+            // no 4 line
+            break 'b;
+        }
+        let line_5 = masks & masks >> DIRECTIONS;
+        if line_5.reduce_or() != 0 {
+            // has 5+ line
+            return MakeResult::Illegal;
+        }
+        // no 5+ and at least one 4 line
+        masks |= masks << DIRECTIONS;
+        masks |= masks << DIRECTIONS << DIRECTIONS;
+        yugo |= bit;
+        if Simd::splat(yugo).simd_eq(masks).any() {
+            // 4 line of yugos
+            return MakeResult::Igo;
+        }
+        migo &= !masks.reduce_or();
+        score += unsafe { _mm256_movemask_pd(transmute(masks)) }.count_ones() as i32;
     }
-    let line_5 = masks & masks >> DIRECTIONS;
-    if line_5.reduce_or() != 0 {
-        return MakeResult::Illegal;
-    }
-    masks |= masks << DIRECTIONS;
-    masks |= masks << DIRECTIONS << DIRECTIONS;
-    new_yugo |= bit;
-    // optimize to vptest
-    if Simd::splat(new_yugo).simd_eq(masks).to_int().reduce_or() != 0 {
-        return MakeResult::GameOver(Outcome::Win);
-    }
-    new_migo &= !masks.reduce_or();
-    new_score += unsafe { _mm256_movemask_pd(transmute(masks)) }.count_ones() as i32;
-    n.opp_migo = new_migo;
-    n.opp_yugo = new_yugo;
-    n.score = c.score;
-    // todo
+    MakeResult::Ok { migo, yugo, score }
 }
 
 pub enum MakeResult {
-    Ok,
+    Ok { migo: u64, yugo: u64, score: i32 },
     Illegal,
-    GameOver(Outcome),
-}
-
-pub enum Outcome {
-    Win,
-    Draw,
-    Loss,
+    Igo,
 }
