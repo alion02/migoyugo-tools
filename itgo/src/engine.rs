6335c8178432672e1a2479fa20e1b32067168a76
alion02 <alojczek@gmail.com>
2026-01-24 05:43:49 +0100

    Make k_ps outputs more accurate

--- itgo/src/engine.rs
+++ itgo/src/engine.rs
@@ -138,11 +138,12 @@ pub fn start() -> SyncSender<Cmd> {
                             Ok((eval, mv)) => {
                                 best = Sq::from_raw(mv);
                                 let eval = Eval::Score(eval); // TODO: convert properly
-                                let time = global.elapsed();
+                                let time_ns = global.started_at.elapsed().as_nanos().max(1);
+                                let time = (time_ns / 1_000_000) as u64;
                                 let nodes = thread.nodes;
-                                let knps = if time == 0 { nodes } else { nodes / time };
                                 let evals = thread.evals;
-                                let keps = if time == 0 { evals } else { evals / time };
+                                let knps = (nodes as u128 * 1_000_000 / time_ns) as u64;
+                                let keps = (evals as u128 * 1_000_000 / time_ns) as u64;
                                 send(&EngineMsg::Info {
                                     pv: vec![best.unwrap()],
                                     eval,
