f91733efea1cd2f9e8f16d930cef317bacb9e56e
alion02 <alojczek@gmail.com>
2026-02-20 06:43:15 +0100

    Fix time management

--- itgo/src/shared.rs
+++ itgo/src/shared.rs
@@ -38,11 +38,11 @@ impl Shared {
     pub fn go(&mut self, started_at: Instant, mut limits: Limits) {
         if let Some(clock) = &limits.clock {
             const K: f64 = 0.05;
-            const EXPECTED_LEN: i32 = 120;
+            const EXPECTED_LEN: i32 = 100;
             const EXPECTED_DELAY: f64 = 10.;
             const MIN_TIME: f64 = 10.;
             let expected_plies_left = (1. + E.powf((EXPECTED_LEN - self.game.frame_ptr().ply) as f64 * K)).ln() / K;
-            let target_time_fraction = 1. / expected_plies_left;
+            let target_time_fraction = 2. / expected_plies_left.max(2.);
             let expected_time_available =
                 clock.left as f64 - EXPECTED_DELAY + clock.incr as f64 * expected_plies_left.floor();
             let computed_limit = (expected_time_available * target_time_fraction).max(MIN_TIME) as u64;
